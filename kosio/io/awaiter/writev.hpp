9d92d891f10d971b0d9035c830a7a796a5b2d131
LAN40 <sdfk32@outlook.com>
2025-09-25 22:31:35 +0800

    添加writev

---
+++ kosio/io/awaiter/writev.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include "kosio/io/base/registrator.hpp"
+
+namespace kosio::io {
+namespace detail {
+    struct WriteV : public IoRegistrator<WriteV> {
+    private:
+        using Super = IoRegistrator<WriteV>;
+
+    public:
+        WriteV(int fd, const struct iovec *iovecs, unsigned nr_vecs, __u64 offset, int flags = 0)
+            : Super{io_uring_prep_writev2, fd, iovecs, nr_vecs, offset, flags} {}
+
+        auto await_resume() const noexcept -> Result<std::size_t> {
+            if (this->cb_.result_ >= 0) [[likely]] {
+                return static_cast<std::size_t>(this->cb_.result_);
+            } else {
+                return ::std::unexpected{make_error(-this->cb_.result_)};
+            }
+        }
+    };
+} // namespace detail
+[[REMEMBER_CO_AWAIT]]
+static inline auto
+writev(int fd, const struct iovec *iovecs, unsigned nr_vecs, __u64 offset, int flags = 0) {
+    return detail::WriteV{fd, iovecs, nr_vecs, offset, flags};
+}
+} // namespace kosio::io
