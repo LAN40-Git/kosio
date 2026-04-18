459a9127988cc3e7477726e536f5e48b5fc61d7b
lan <sdfk32@outlook.com>
2026-04-18 19:12:47 +0800

    2026-4-18 19:12

--- kosio/sync.hpp
+++ kosio/sync.hpp
@@ -4,4 +4,4 @@
 #include "kosio/sync/semaphore.hpp"
 #include "kosio/sync/shared_mutex.hpp"
 #include "kosio/sync/condition_variable.hpp"
-#include "kosio/sync/spsc_queue.hpp"
+#include "kosio/sync/channel.hpp"
